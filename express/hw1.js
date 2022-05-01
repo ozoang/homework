4b40c95fc960ac5be22dfaab9479d5b1746f5c2c
supara <suppara00k@gmail.com>
2022-05-01 21:45:14 +0700

    first

--- express/hw1.js
+++ express/hw1.js
@@ -7,16 +7,15 @@ app.get('/cal', (req, res) => {
     let num2 = Number(req.query.b);
     let num3 = Number(req.query.c);
     let num4 = Number(req.query.d);
-    if ((num1 >= 1 && num1 <= 9) ||
-        (num2 >= 1 && num2 <= 9) ||
-        (num3 >= 1 && num3 <= 9) ||
-        (num4 >= 1 && num4 <= 9)
+    if (num1 >= 1 && num1 <= 9 &&
+        num2 >= 1 && num2 <= 9 &&
+        num3 >= 1 && num3 <= 9 &&
+        num4 >= 1 && num4 <= 9
     ) {
-
-        console.log('in')
-        res.send('in');
+        // console.log('in')
+        res.send();
     } else {
-        console.log('out')
+        // console.log('out')
         res.status(403).send('Forbidden');
     }
 
