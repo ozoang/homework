8871cd07c756390969271d51a3f2b48e686d9d57
supara <suppara00k@gmail.com>
2022-05-02 11:47:22 +0700

    http commit

--- express/hw1.js
+++ express/hw1.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const solve24game = require('24game-solver/dist/24game-solver');
 
 app.get('/cal', (req, res) => {
     let num1 = Number(req.query.a);
@@ -13,14 +14,22 @@ app.get('/cal', (req, res) => {
         num4 >= 1 && num4 <= 9
     ) {
         // console.log('in')
-        res.send();
+        const result = solve24game([num1, num2, num3, num4], 24);
+        let A = '';
+        for (i = 0; i < result.length; i++) {
+
+            A += result[i] + ',\n'
+
+        }
+        if (result.length == 0) {
+            res.send('Fail');
+        } else {
+            res.send('Success' + '\n' + A);
+        }
     } else {
         // console.log('out')
         res.status(403).send('Forbidden');
     }
-
-
-
 
 });
 
---
+++ http/hw1.js
@@ -0,0 +1,124 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+const userData = [{
+        firstname: "1",
+        lastname: "1",
+        id: "1",
+        rank: "1",
+        tel: "1",
+        email: "1"
+    },
+    {
+        firstname: "2",
+        lastname: "2",
+        id: "2",
+        rank: "2",
+        tel: "2",
+        email: "2"
+    },
+    {
+        firstname: "3",
+        lastname: "3",
+        id: "3",
+        rank: "3",
+        tel: "3",
+        email: "3"
+    }
+]
+
+let fname;
+let lname;
+let id;
+let rank;
+let tel;
+let email;
+
+app.get('/getData', (req, res) => {
+    res.send({ userData });
+});
+
+app.put('/update', (req, res) => {
+    let idDelete = req.body.iddelete;
+
+
+    fname = req.body.firstname;
+    lname = req.body.lastname;
+    id = req.body.id;
+    rank = req.body.rank;
+    tel = req.body.tel;
+    email = req.body.email;
+
+    // console.log({ userData })
+    console.log(idDelete)
+
+    for (let i = 0; i < userData.length; i++) {
+        if (email === userData[i].email || tel === userData[i].tel) {
+            return res.status(400).send("Bad Request");
+        }
+        console.log(i)
+        console.log(userData[i])
+        if (idDelete == userData[i].id) {
+            userData.splice(i, 1, {
+                firstname: fname,
+                lastname: lname,
+                id: id,
+                rank: rank,
+                tel: tel,
+                email: email
+            });
+            return res.send('update success');
+        }
+    }
+    return res.status(400).send("Bad Request");
+
+});
+
+app.post('/create', (req, res) => {
+    fname = req.body.firstname;
+    lname = req.body.lastname;
+    id = req.body.id;
+    rank = req.body.rank;
+    tel = req.body.tel;
+    email = req.body.email;
+
+    for (let i = 0; i < userData.length; i++) {
+
+        if (email === userData[i].email || tel === userData[i].tel) {
+            return res.status(400).send("Bad Request");
+        }
+    }
+    userData.push({
+        firstname: fname,
+        lastname: lname,
+        id: id,
+        rank: rank,
+        tel: tel,
+        email: email
+    })
+
+    // console.log({ userData })
+    return res.send("Add datasuccess");
+});
+
+app.delete('/delete', (req, res) => {
+
+    id = req.body.id;
+    for (let i = 0; i < userData.length; i++) {
+        console.log(userData[i])
+        if (id == userData[i].id) {
+            userData.splice(i, 1);
+            return res.send('delete success');
+        }
+    }
+    return res.status(400).send("Bad Request");
+
+
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
